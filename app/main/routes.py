64aaefa292e41c4fb232e678ec816966ccc17793
RainTwo <wangzhe92824@qq.com>
2021-07-09 13:02:03 +0800

    javascript细节

--- app/main/routes.py
+++ app/main/routes.py
@@ -138,6 +138,13 @@ def translate_text():
                                       request.form['dest_language'])})
 
 
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user_popup.html', user=user)
+
+
 @bp.route('/search')
 @login_required
 def search():
