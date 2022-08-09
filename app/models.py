fa017f9248cf7f485a50d9f614956e5f927e1aba
RainTwo <wangzhe92824@qq.com>
2022-08-09 14:05:11 +0800

    final

--- app/models.py
+++ app/models.py
@@ -120,5 +120,15 @@ db.event.listen(db.session, 'before_commit', Post.before_commit)
 db.event.listen(db.session, 'after_commit', Post.after_commit)
 
 
+class Message(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	body = db.Column(db.String(140))
+	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
+	def __repr__(self):
+		return '<Message {}>'.format(self.body)
+		
+		
 		
